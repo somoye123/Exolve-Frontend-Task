220278ffe892ff9403cf5e500ddf3e192855f693
somoye <somoye.ayotunde@gmail.com>
2019-08-27 07:42:09 -0700

    Add unique content to the modal

--- script/main.js
+++ script/main.js
@@ -6,10 +6,27 @@ $(document).ready(function (params) {
         card.setAttribute('data-target', '#exampleModal')
         card.addEventListener('click',()=> displayInfo(card))
     })
-    console.log(allCards);
-    
 })
 
 const displayInfo = (data) => {
-    console.log(data, 'hello');
+   const imgSrc = getImgSrc(data);
+   const title = getTitle(data);
+   const pres = getPres(data);
+   const modalImg = document.querySelector('#exampleModal img');
+   const modalTitle = document.querySelector('#exampleModal .modal-title');
+   const modalPres = document.querySelector('#exampleModal .modal-pres');
+   modalImg.setAttribute('src', imgSrc);
+   modalTitle.textContent = title;
+   modalPres.textContent = pres
+
+}
+const getImgSrc = (data) => {
+    const imgSrc = data.querySelector('img').getAttribute('src')
+    return imgSrc;
+}
+const getTitle = (data) => {
+    return data.querySelector('.card-title').textContent;
+}
+const getPres = (data) => {
+    return data.querySelector('.card-text').textContent;
 }
